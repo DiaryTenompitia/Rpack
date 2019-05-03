ab060c3a745d4f9bbe0eaf875e44b37d2ddd2ef9
DiaryTenompitia <gasparddiary@gmail.com>
2019-05-03 11:14:52 -0400

    first commit

---
+++ R/f.R
@@ -0,0 +1,18 @@
+#'
+#' This is a title
+#'
+#' @param x This is the first argument
+#' @param y This is the second argument
+#'
+#' @return Return a numeric value
+#' @export
+#'
+#' @examples
+#' f(2,3)
+#'
+
+f <- function(x, y) {
+  x * y
+}
+
+
